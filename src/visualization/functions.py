b7488bbd9ea8056e1e64ddf97842fca44b5b77da
Ivan Osipchyk <ivanosipchyk@Ivans-MacBook-Air.local>
2023-03-31 14:48:06 +0800

    added file with support functions for visualizations

---
+++ src/visualization/functions.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+def read_files(cities_, years_):
+    path = '../../data/cleaned/'
+    dataframes_dict = {}
+
+    for city in cities_:
+        for year in years_:
+            df = pd.read_csv(path + city.capitalize() + '/' + city + '_' + year + '.csv')
+            dataframes_dict[city + '_' + year] = df
+
+    return dataframes_dict
+
+
+def remove_columns(dataframes_dict):
+    columns = ['author', 'author_created_utc', 'body', 'subreddit', 'Segment']
+    dataframes_dict_copy = dataframes_dict
+    for key, value in dataframes_dict_copy:
+        df = value.drop(columns, axis=1)
+        dataframes_dict[key] = df
+
+    return dataframes_dict
